43ec7e683d96da0aa432c1bdf7b8801f2e11426f
shalantor <protipas2@hotmail.com>
2017-03-08 13:39:02 +0200

    Add routes for our application

---
+++ app/routes.js
@@ -0,0 +1,45 @@
+/*Route configuration*/
+
+module.exports = function(app,passport) {
+
+  /*HOME PAGE*/
+  app.get('/', function(req,res){
+    res.render('index.ejs');
+  });
+
+  /*LOGIN FORM*/
+  app.get('/login', function(req,res){
+    res.render('login.ejs', {message : req.flash('loginMessage') }); //render flash data if there is any
+  });
+
+  /*SIGNUP FORM*/
+  app.get('/signup', function(req,res){
+    res.render('signup.ejs', {message : req.flash('signupMessage')}); //render flash message data if there is any
+  });
+
+  /*PROFILE PAGE*/
+  app.get('/profile', isLoggedIn, function(req,res){
+    res.render('profile.ejs',{
+      user : req.user     //Get user , with his login data
+    });
+  });
+
+  /*LOG OUT PAGE*/
+  app.get('/logout', function(req,res){
+    req.logout();
+    res.redirect('/');
+  });
+
+};
+
+//Check if user is logged in
+function isLoggedIn(req,res,next){
+
+  //If user is logged in continue
+  if (req.isAuthenticated()){
+    return next();
+  }
+
+  //Else redirect to HOME
+  res.redirect('/');
+}
