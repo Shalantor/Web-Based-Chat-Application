202eb118af20f55e54e10b22d167cc841acd8950
shalantor <protipas2@hotmail.com>
2017-03-22 09:25:18 +0200

    Add method to check if a particular user is online

--- app/helper.js
+++ app/helper.js
@@ -7,54 +7,65 @@ class Helper{
   /*Method to set user online status to N (means NO)*/
   setUserStatusOffline(localReq,facebookReq,googleReq){
 
-    console.log(localReq);
-    console.log(facebookReq);
-    console.log(googleReq);
-
     /*Check for type of user*/
     /*Local user*/
     if ( localReq.username ){
       this.User.findOneAndUpdate({ 'local.username' : localReq.username }, {'local.online' : "N"} , function(err,user) {
-        console.log("I AM IN LOCAL");
         /*If there are any errros , return the error*/
         if (err){
           return err;
         }
-
-        /*Set user status offline*/
-        console.log("ONLINE STATUS IS SET TO " + user.local.online);
-
       });
     }
     else if ( facebookReq.id ){
-
       this.User.findOneAndUpdate({ 'facebook.id' : facebookReq.id }, {'facebook.online' : "N"} , function(err,user) {
-        console.log("I AM IN FACEBOOK");
         /*If there are any errros , return the error*/
         if (err){
           return err;
         }
-
-        /*Set user status offline*/
-        console.log("ONLINE STATUS IS SET TO " + user.facebook.online);
-
       });
-
     }
     else if ( googleReq.id  ){
       this.User.findOneAndUpdate({ 'google.id' : googleReq.id }, {'google.online' : "N"} , function(err,user) {
-        console.log("I AM IN GOOGLE");
         /*If there are any errros , return the error*/
         if (err){
           return err;
         }
-
-        /*Set user status offline*/
-        console.log("ONLINE STATUS IS SET TO " + user.google.online);
-
       });
     }
+  }
 
+  /*Check if given user is online*/
+  isUserOnline(userReq){
+    /*Check for type of user*/
+    /*Local user*/
+    if ( userReq.local.username ){
+      this.User.findOne({ 'local.username' : userReq.local.username } , function(err,user) {
+        /*If there are any errros , return the error*/
+        if (err){
+          return err;
+        }
+        return user.local.online === "Y";
+      });
+    }
+    else if ( userReq.facebook.id ){
+      this.User.findOne({ 'facebook.id' : userReq.facebook.id } , function(err,user) {
+        /*If there are any errros , return the error*/
+        if (err){
+          return err;
+        }
+        return user.facebook.online === "Y";
+      });
+    }
+    else if ( googleReq.id  ){
+      this.User.findOne({ 'google.id' : userReq.google.id }, function(err,user) {
+        /*If there are any errros , return the error*/
+        if (err){
+          return err;
+        }
+        return user.google.online === "Y";
+      });
+    }
   }
 
 
