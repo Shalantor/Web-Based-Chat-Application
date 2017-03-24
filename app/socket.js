4da8f403aac8a1e427b2dc8e8e8fdc4a2b1a7272
shalantor <protipas2@hotmail.com>
2017-03-24 16:06:02 +0200

    On connection, user socket id gets stored , NOTE: must find way to send user data on connection

--- app/socket.js
+++ app/socket.js
@@ -1,15 +1,34 @@
+var helper = require('./helper');
+
 /*Initialize socket io server*/
 var init = function(app){
 
   var server = require('http').Server(app);
   var io = require('socket.io')(server);
 
+
   io.on('connection', function(socket){
-    console.log('user connected');
+
+    /*The logs just exist for testing the application*/
+    console.log(socket.client.request);
+
+    /*When user connects store his socket.id in database, for future socket operations*/
+    helper.addSocketId(socket.request.user,socket,function(user,err){
+      if (err){
+        console.log('An error occurred');
+      }
+      else{
+        console.log('Everything is ok!');
+      }
+    });
+
     socket.on('disconnect',function(){
       console.log('user disconnected');
     });
-    socket.on('chat message', function(msg){
+
+    /*The user wants to search for another user based on the name he typed
+    So search the database for the name and return the users that were found*/
+    socket.on('find-user', function(msg){
       io.emit('chat message', msg);
     });
   });
